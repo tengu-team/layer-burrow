54c97829b2081333b8e367211fd61a2f83f9f7f9
sborny <sander.borny@ugent.be>
2018-05-14 10:49:23 +0200

    Use burrow v1.0

--- reactive/burrow.py
+++ reactive/burrow.py
@@ -14,12 +14,14 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import os
+import wget
 from subprocess import call
 from jujubigdata import utils
-from subprocess import call
+from subprocess import call, run, CalledProcessError
+from charms.reactive.helpers import data_changed
 from charmhelpers.core import hookenv, templating, host
 from charmhelpers.core.hookenv import status_set, log, open_port, close_port
-from charms.reactive import when, when_not, set_state, remove_state, when_any
+from charms.reactive import when, when_not, set_flag, clear_flag, when_any
 from charms.layer.go import go_environment
 
 
@@ -29,21 +31,31 @@ config = hookenv.config()
 @when('go.installed')
 @when_not('burrow.installed')
 def install_burrow():
+    # Install dep https://github.com/golang/dep
+    url = "https://raw.githubusercontent.com/golang/dep/master/install.sh"
+    wget.download(url=url, out='/home/ubuntu/dep-installer.sh')
+    os.chmod('/home/ubuntu/dep-installer.sh', 0o755)
+    try:
+        #output = run('/home/ubuntu/dep-installer.sh')
+        #output.check_returncode() 
+        utils.run_as('root', '/home/ubuntu/dep-installer.sh')
+    except CalledProcessError as e:
+        log(e)
+        status_set('blocked', 'Failed to install dep.')
+        return
+    
     previous_wd = os.getcwd()
     go_env = go_environment()
     utils.run_as('ubuntu', 'go', 'get', 'github.com/linkedin/Burrow')
     os.chdir(go_env['GOPATH'] + '/src/github.com/linkedin/Burrow')
-    # Temporary fix for https://github.com/linkedin/Burrow/issues/222
-    with open(go_env['GOPATH'] + '/src/github.com/linkedin/Burrow/Godeps', 'a') as f:
-        f.write('github.com/klauspost/crc32          1bab8b35b6bb565f92cbc97939610af9369f942a')
-    utils.run_as('ubuntu', 'gpm', 'install')
+    utils.run_as('ubuntu', 'dep', 'ensure')
     utils.run_as('ubuntu', 'go', 'install')
     dirs = ['/home/ubuntu/burrow', '/home/ubuntu/burrow/log', '/home/ubuntu/burrow/config']
     for dir in dirs:
         if not os.path.exists(dir):
             os.makedirs(dir)
     os.chdir(previous_wd)
-    set_state('burrow.installed')
+    set_flag('burrow.installed')
 
 
 @when_not('kafka.ready')
@@ -55,18 +67,14 @@ def status_kafka():
 def config_changed_port():
     stop_burrow()
     if config.previous('port'):
-        close_port('port')
-    remove_state('burrow.configured')
+        close_port(config.previous('port'))
+    clear_flag('burrow.configured')
 
 
-@when_any('config.changed.slack-notifier',
-          'config.changed.slack-webhook',
-          'config.changed.consumer-groups',
-          'config.changed.notify-interval')
+@when_any('config.changed.consumer-groups')
 def config_changed_slack():
-    # Extra conditions
     stop_burrow()
-    remove_state('burrow.configured')
+    clear_flag('burrow.configured')
 
 
 @when('burrow.started')
@@ -80,7 +88,7 @@ def stop_burrow():
     if host.service_running('burrow'):
         call(['systemctl', 'disable', 'burrow'])
         host.service_stop('burrow')
-    remove_state('burrow.started')
+    clear_flag('burrow.started')
 
 
 @when('go.installed', 'kafka.ready')
@@ -98,13 +106,13 @@ def configure(kafka):
         'logdir': '/home/ubuntu/burrow/log',
         'logconfig': '/home/ubuntu/burrow/config/logging.cfg',
         'api_port': config.get('port'),
-        'slack': config.get('slack-notifier')
     }
 
-    zookeeper_nodes = []
+    zookeeper_nodes = {}
     zookeeper_port = 2181
     for zookeeper_unit in kafka.zookeepers():
-        zookeeper_nodes.append(zookeeper_unit['host'])
+        # Use dict to prevent duplicate zoo info from the kafka interface.
+        zookeeper_nodes[zookeeper_unit['host']] = ""
         zookeeper_port = zookeeper_unit['port']
 
     kafka_nodes = []
@@ -113,7 +121,7 @@ def configure(kafka):
         kafka_nodes.append(kafka_unit['host'])
         kafka_port = kafka_unit['port']
 
-    context['zoo_units'] = zookeeper_nodes
+    context['zoo_units'] = zookeeper_nodes.keys()
     context['zoo_port'] = zookeeper_port
     context['kafka_units'] = kafka_nodes
     context['kafka_port'] = kafka_port
@@ -124,24 +132,16 @@ def configure(kafka):
         break
     context['cluster'] = kafka_cluster_name
 
-    if (config.get('slack-notifier') and
-            config.get('slack-webhook').rstrip() and
-            config.get('consumer-groups').rstrip() and
-            config.get('slack-channel').rstrip() and
-            config.get('notify-interval').rstrip()):
-        context['slack_webhook'] = config.get('slack-webhook').rstrip()
-        context['groups'] = config.get('consumer-groups').rstrip().split(' ')
-        context['channel'] = config.get('slack-channel').rstrip()
-        context['notify_interval'] = config.get('notify-interval').rstrip()
+    context['groups'] = config.get('consumer-groups').rstrip().split(' ')
 
     templating.render(source='burrow-conf.tmpl',
-                      target='/home/ubuntu/burrow/config/burrow.cfg',
+                      target='/home/ubuntu/burrow/config/burrow.toml',
                       context=context)
 
     go_env = go_environment()
     systemd_context = {
         'burrow_path': go_env['GOPATH'] + '/bin/Burrow',
-        'config_path': '/home/ubuntu/burrow/config/burrow.cfg',
+        'config_path': '/home/ubuntu/burrow/config',
     }
 
 
@@ -150,7 +150,7 @@ def configure(kafka):
                       context=systemd_context)
 
     open_port(config.get('port'))
-    set_state('burrow.configured')
+    set_flag('burrow.configured')
 
 
 @when('burrow.configured', 'kafka.ready')
@@ -161,9 +161,16 @@ def start(kafka):
         call(['systemctl', 'enable', 'burrow'])
         host.service_start('burrow')
     status_set('active', 'ready (:' + str(config.get('port')) + ')')
-    set_state('burrow.started')
+    set_flag('burrow.started')
 
 
 @when('http.available')
 def configure_http(http):
     http.configure(port=config.get('port'))
+
+
+@when('kafka.ready')
+def check_kafka_changed(kafka):
+    if data_changed('kafka_info', kafka.kafkas()):
+        stop_burrow()
+        clear_flag('burrow.configured')
